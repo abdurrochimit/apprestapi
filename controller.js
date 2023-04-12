9798648054d2f3f50f8acff1b3e8a97838bba662
abdurrochim <abdurrochim.it@gmail.com>
2023-04-12 09:35:05 +0700

    get mahasiswa

--- controller.js
+++ controller.js
@@ -3,17 +3,29 @@
 var response = require('./res');
 var connection = require('./koneksi');
 
-exports.index = function (req,res) {
+exports.index = function (req, res) {
     response.ok("Aplikasi Rest Api ku Berjalan", res)
 }
 
 //  Menampilkan semua data mahasiswa
-exports.tampilsemuamahasiswa =  function(req, res){
-    connection.query('SELECT * FROM mahasiswa', function(error,rows,fields){
+exports.tampilsemuamahasiswa = function (req, res) {
+    connection.query('SELECT * FROM mahasiswa', function (error, rows, fields) {
         if (error) {
             connection.log(error)
-        }else{
-            response.ok(rows,res)
+        } else {
+            response.ok(rows, res)
         }
     });
+};
+
+// Menampilkan mahasiswa berdasarkan id
+exports.tampilberdasarkanid = function (req, res) {
+    let id = req.params.id;
+    connection.query('SELECT * FROM mahasiswa WHERE id_mahasiswa = ?', [id], function (error, rows, fields) {
+        if (error) {
+            connection.log(error)
+        } else {
+            response.ok(rows, res)
+        }
+    })
 }
