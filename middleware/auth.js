d9946727c67ede5f7db82a725b6ff8b8a4d43363
abdurrochim <abdurrochim.it@gmail.com>
2023-04-27 17:28:44 +0700

    registrasiberhasil

--- middleware/auth.js
+++ middleware/auth.js
@@ -7,37 +7,37 @@ var config = require('../config/secret');
 var ip = require('ip');
 
 // Controller Register
-exports.registrasi = function(req,res){
+exports.registrasi = function (req, res) {
     var post = {
-        username : req.body.username,
-        email : req.body.email,
-        password : md5(req.body.password),
-        role : req.body.role,
-        tanggal_daftar : new Date()
+        username: req.body.username,
+        email: req.body.email,
+        password: md5(req.body.password),
+        role: req.body.role,
+        tanggal_daftar: new Date()
     }
 
-    var query = "SELECT email FROM ?? WHERE ??";
+    var query = "SELECT email FROM ?? WHERE ??=?";
     var table = ["user", "email", post.email];
 
-    query = mysql.format(query,table);
+    query = mysql.format(query, table);
 
-    connection.query(query, function(error, rows){
+    connection.query(query, function (error, rows) {
         if (error) {
             console.log(error);
-        }else{
+        } else {
             if (rows.length == 0) {
                 var query = "INSERT INTO ?? SET ?";
                 var table = ["user"];
                 query = mysql.format(query, table);
-                connection.query(query, post, function(error, rows){
-                    if(error){
+                connection.query(query, post, function (error, rows) {
+                    if (error) {
                         console.log(error);
-                    }else{
+                    } else {
                         response.ok("Berhasil menambahkan data user baru", res);
                     }
                 });
-            }else{
-                response.ok("Email sudah terdaftar");
+            } else {
+                response.ok("Email sudah terdaftar", res);
             }
         }
     })
--- server.js
+++ server.js
@@ -1,15 +1,21 @@
-const express = require('express')
-const bodyParser = require('body-parser')
-const app = express()
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var morgan = require('morgan');
+const app = express();
 const port = 3000
 
 // parse application/json
 app.use(bodyParser.urlencoded({extended: true}));
 app.use(bodyParser.json());
+app.use(morgan('dev'))
 
 // panggil routers
 var routes = require('./routes');
 routes(app);
+
+// daftarkan menu routes dari index
+app.use('/auth', require('./middleware'));
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
